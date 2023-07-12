008c99fa0af5e01a0c51726a651a4b1062df5f60
Yuborama <davidjohan2@hotmail.com>
2023-07-11 19:22:02 -0500

    clean

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,12 +16,7 @@ const loc = ref<{
   lat: null,
   long: null,
 });
-
-const changeUrl = ref(false);
-const closeEvent = ref(false);
 const tokennotification = ref<string | null>(null);
-
-const miIframe = ref<HTMLIFrameElement>(null as any);
 const getCurrentPosition = async () => {
   const pos = await Geolocation.getCurrentPosition();
   loc.value = {
@@ -31,32 +26,18 @@ const getCurrentPosition = async () => {
 };
 const openWebView = () => {
   console.log("openWebView");
-  // InAppBrowser.openWebView({
-  //   url: "https://www.google.com",
-  //   title: "mi casita muy bonita",
-  // });
   InAppBrowser.open({
     url: "https://www.google.com",
   });
 
   InAppBrowser.addListener("closeEvent", (event) => {
-    closeEvent.value = true;
     alert("loadstart: " + JSON.stringify(event));
   });
   InAppBrowser.addListener("urlChangeEvent", (event) => {
-    changeUrl.value = true;
     alert("urlChangeEvent: " + JSON.stringify(event));
   });
 };
 
-const getInfo = () => {
-  if (miIframe.value !== null) {
-    console.log("myIframe location", miIframe.value.contentWindow?.location);
-    console.log("myIframe contentWindows", miIframe.value.contentWindow);
-    console.log("myIframe onload", miIframe.value.onload);
-    console.log("myIframe value", miIframe.value);
-  }
-};
 const logDeviceInfo = async () => {
   const info = await Device.getInfo();
 
@@ -70,34 +51,7 @@ const logDeviceInfo = async () => {
   return `${info.platform}-${userAgent}`;
 };
 
-watch(
-  miIframe,
-  () => {
-    if (miIframe.value !== null) {
-      console.log("myIframe", miIframe.value.contentWindow?.location);
-    }
-  },
-  { immediate: true, deep: true }
-);
-const loadstart = (event: any) => {
-  console.log("loadstart", event);
-  // alert("loadstart: " + JSON.stringify(event));
-};
-
 onMounted(() => {
-  const iframe = miIframe;
-
-  // Agregamos un listener para el evento 'load'
-  // iframe.addEventListener("load", () => {
-  //   // Obtenemos la URL actual dentro del iframe
-  //   const url = iframe.contentWindow.location.href;
-
-  //   // Imprimimos la URL en la consola
-  //   console.log(url);
-  // });
-  // Request permission to use push notifications
-  // iOS will prompt user and return if they granted permission or not
-  // Android will just grant without prompting
   PushNotifications.requestPermissions().then((result) => {
     if (result.receive === "granted") {
       // Register with Apple / Google to receive push via APNS/FCM
@@ -142,21 +96,6 @@ onMounted(() => {
     <input type="text" v-model="tokennotification" />
     <button @click="getCurrentPosition">Get Current Location</button>
     <button @click="openWebView">Open WebView</button>
-    <p v-if="changeUrl">changeUrl: {{ changeUrl }}</p>
-    <p v-if="closeEvent">closeEvent: {{ closeEvent }}</p>
     <button @click="logDeviceInfo">get info</button>
-
-    <!-- <iframe
-      ref="miIframe"
-      src="https://es.wikipedia.org/wiki/Geolocalizaci%C3%B3n"
-      style="height: 500px; width: 100%"
-      @onload="loadstart"
-      @loadstart="loadstart"
-      @change="loadstart"
-      width="900"
-      height="315"
-      frameborder="0"
-      allowfullscreen
-    /> -->
   </div>
 </template>
